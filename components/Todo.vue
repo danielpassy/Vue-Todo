8909850f4530742d41a707e442509e296947c282
daniel passy <danielpassy@hotmail.com>
2021-02-13 20:57:23 -0300

    create nuxt project

---
+++ components/Todo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div id="flex">
+      {{ description }} asdasd
+      <form><input v-model="checked" type="checkbox" /></form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todo",
+  props: ["description"],
+  data() {
+    return {
+      checked: true,
+    };
+  },
+  methods: {
+    deleteTodo() {
+      this.$emit("grito", "1");
+    },
+  },
+};
+</script>
+
+<style scoped>
+#flex {
+  display: flex;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <div id="flex">
+      <form>
+        <input
+          type="text"
+          placeholder="Insert your Todo"
+          v-model="todoInput"
+          name="todo"
+          id="todo"
+        />
+      </form>
+      <input type="submit" @click="alertUser" />
+    </div>
+    <Todo @grito="removeTodo" description="shalom" />
+
+    <div class="todos"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  componentes: {},
+  data() {
+    return {
+      todoInput: "",
+      todos: [],
+    };
+  },
+  methods: {
+    alertUser(e) {
+      this.todos.push(this.todoInput);
+      this.todoInput = "";
+    },
+    removeTodo() {
+      alert("someone attempted to delete this");
+    },
+  },
+};
+</script>
+
+<style>
+#flex {
+    display:flex
+}
+.container{
+    align-items: center;
+    width: 80vw;
+
+}
+</style>
